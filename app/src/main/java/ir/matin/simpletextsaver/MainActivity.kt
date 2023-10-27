f4bbf6bafed4752e7d42f33593840bce57f0cf72
matin <138515matin@gmail.com>
2023-10-27 19:01:05 +0330

    Add ScreenSplash !

--- app/src/main/java/ir/matin/simpletextsaver/MainActivity.kt
+++ app/src/main/java/ir/matin/simpletextsaver/MainActivity.kt
@@ -1,11 +1,28 @@
 package ir.matin.simpletextsaver
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            val intent=Intent(this@MainActivity , MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+
+
+        },2000)
     }
 }
